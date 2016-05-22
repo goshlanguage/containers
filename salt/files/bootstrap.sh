0e95a95937acd5242eef11f960c41a92da1e8ee0
ryanhartje <ryan@ryanhartje.com>
2016-05-22 17:24:23 -0500

    getting salty

--- salt/files/bootstrap.sh
+++ salt/files/bootstrap.sh
@@ -10,4 +10,6 @@ git clone https://github.com/saltstack/salt;
 cd salt;
 pip install -r requirements/base.txt;
 pip install -r requirements/zeromq.txt;
+cp /usr/local/src/salt/conf/master /etc/salt/
+cp /usr/local/src/salt/conf/minion /etc/salt/
 python setup.py install;
