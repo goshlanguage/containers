423e2469794706e9e8713ec1c5a99c7d8601273d
Ryan Hartje <ryan@ryanhartje.com>
2018-07-23 15:34:20 -0500

    update to gluster container

--- gluster/bin/init.sh
+++ gluster/bin/init.sh
@@ -2,4 +2,10 @@
 
 rpcbind start
 service glusterfs-server start
-tail -f /var/log/glusterfs/gluster.log /var/log/glusterfs/nfs.log
+sleep 1
+ls /var/log/glusterfs
+if [ -f /var/log/glusterfs/nfs.log ] ; then
+  tail -f /var/log/glusterfs/glusterd.log
+else
+  tail -f /var/log/glusterfs/glusterd.log /var/log/glusterfs/nfs.log
+fi
